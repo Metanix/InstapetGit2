c85ae417cbeebfc1175227dfd80db227d3252351
Metanix <metanix.18@gmail.com>
2015-06-23 15:26:30 -0300

    cambio de nombres
    xq el profe lo pidio

--- models/imagen.js
+++ models/imagen.js
@@ -9,15 +9,21 @@ var imagenSchema = mongoose.Schema({
   	passLogin: {type: String, required:false},  
   
 //Registro
- 	nameReg:  {type: String, required:false},
-  	apeReg:  {type: String, required:false},
-  	mailReg: {type: String, required:false},
-  	passReg: {type: String, required:false},
-  	nickReg: {type: String, required:false},
+ 	nameRegistro:  {type: String, required:false},
+  	apeRegistro:  {type: String, required:false},
+  	mailRegistro: {type: String, required:false},
+  	passRegistro: {type: String, required:false},
+  	nickRegistro: {type: String, required:false},
 
 //Mascotas
 	nameMascota:  {type: String, required:false},
 	amigosMascota:  {type: String, required:false},
+
+//fotos
+	urlImagen:  {type: String, required:false},
+	sizeImagen:  {type: String, required:false},
+
+
 });
 
 module.exports = mongoose.model('Imagen', imagenSchema );
